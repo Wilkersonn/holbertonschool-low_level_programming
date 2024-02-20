74e988d82f23a4c0750bad916d458af7427a9611
Wilkersonn <remyfournieroff@gmail.com>
2024-02-20 14:35:52 +0100

    add new directories

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,9 +11,9 @@ int main(void)
 {
 int n;
 
-srand(time(0));  /* Seed for random number generation */
+srand(time(0));  
 
-n = rand() - RAND_MAX / 2;  /* Assign a random number to n */
+n = rand() - RAND_MAX / 2;  
 
 printf("%d is ", n);
 
