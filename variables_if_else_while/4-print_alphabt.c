ae577304e26e0c8aa5dff330f317d98d503300b9
Wilkersonn <remyfournieroff@gmail.com>
2024-02-21 10:41:44 +0100

    push 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char *message = "abcdefghijklmnopqrstuvwxyz\n";
+const char *ptre = message;
+while (*ptr != 'q' && *ptr != 'e')
+{
+ptr++;
+}
+
+while (*ptr)
+{
+putchar(*ptr);
+ptr++;
+}
+
+
+return (0);
+}
