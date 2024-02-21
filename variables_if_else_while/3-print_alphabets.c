f803f213bb77143837277aa3940824c61578e5ad
Wilkersonn <remyfournieroff@gmail.com>
2024-02-21 10:32:15 +0100

    Push 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char *message = "abcdefghijklmnopqrstuvwxyz\n";
+const char *message1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+
+while (*message)
+{
+putchar(*message);
+message++;
+}
+while (*message1)
+{
+putchar(*message1);
+message1++;
+}
+
+return (0);
+}
