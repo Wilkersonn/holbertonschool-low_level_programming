fcd0d7f6654407af2c8741b6210d0abdbe3c9e4f
Wilkersonn <remyfournieroff@gmail.com>
2024-02-24 23:34:31 +0100

    push

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_islower - checks for lower case
+*@c: the character in question
+*Return: 1 if c is lowercase, 0 otherwise
+*/
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
