eb5cf9fcd57309f3b4c27e802d00e2c48dcdfc5f
Wilkersonn <remyfournieroff@gmail.com>
2024-02-26 11:34:26 +0100

    Push Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * This function multiplies two integers and returns the result.
+ * Return: The product of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,5 @@
+#include <stdio.h>
+#include <unistd.h>
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
