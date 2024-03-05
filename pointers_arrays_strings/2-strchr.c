e9ca7d875932c3c3a73a9e772a830ed32d00640a
Wilkersonn <remyfournieroff@gmail.com>
2024-03-05 14:41:27 +0100

    Push Task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
- * _strchr - Locates the first occurrence of a character in a string.
- * @s: Pointer to the string.
- * @c: Character to locate.
+ * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to find
  *
- * Return: Pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found.
+ * Return: a pointer to the first occurrence
+ * of the character c in the string s, or NULL
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return s;
-        }
-        s++;
-    }
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
 
-    if (*s == c)
-    {
-        return s;
-    }
 
-    return NULL;
+	if (*s == c)
+		return (s);
+
+	return (NULL);
 }
