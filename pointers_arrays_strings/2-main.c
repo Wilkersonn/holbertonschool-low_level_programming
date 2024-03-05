20bd85a76a0ace4ef29bf6e3def52e2b48c13098
Wilkersonn <remyfournieroff@gmail.com>
2024-03-05 14:43:40 +0100

    Push Task 2

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -8,30 +8,14 @@
  */
 int main(void)
 {
-    char *str;
-    int len;
+	char *s = "hello";
+	char *f;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
-}
+	f = _strchr(s, 'l');
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+	return (0);
 }
