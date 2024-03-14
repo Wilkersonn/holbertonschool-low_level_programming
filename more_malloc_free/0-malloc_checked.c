d4bca3431c83f258e4c7a3e55d617b8903a04834
Wilkersonn <remyfournieroff@gmail.com>
2024-03-14 11:31:19 +0100

    Push Task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,18 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
-* malloc_checked - allocates memory using malloc
-* @b:variable
-* Return: Always 0.
-*/
-
+ * *malloc_checked - allocate memory with malloc
+ * @b: unsigned int type
+ * Return: return pointer
+ */
 void *malloc_checked(unsigned int b)
 {
-int *s;
+	void *ptr;
 
-s = malloc(b);
-if (s == NULL)
-exit(98);
-return (s);
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
