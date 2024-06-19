dbda7e9b743696d50dc357c7d06ffda6e6b4407f
Wilkersonn <remyfournieroff@gmail.com>
2024-06-19 11:07:25 +0200

    Push task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,23 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - This is the entry point of the code
+ * print_alphabet - This is the entry point of the code
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("abcdefghijklmnopqrstuvwxyz");
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
 
 	return (0);
 }
