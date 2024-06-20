32053013d8281aecbec2ba4ef21e5d36e63a8e6d
Wilkersonn <remyfournieroff@gmail.com>
2024-06-20 14:10:46 +0200

    Push task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int c = 0;
+
+    while (c <= 9)
+    {
+        putchar(c + '0');
+        c++;
+    }
+
+    putchar('\n');
+
+    return 0;
+}
