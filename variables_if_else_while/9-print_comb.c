cbb45a4f30f59afb833416112a7427a492cf3201
Wilkersonn <remyfournieroff@gmail.com>
2024-06-21 13:43:24 +0200

    Push final task

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int javaisimplementenviedemettreunevariableextremementlongue = 0;
+
+    while (javaisimplementenviedemettreunevariableextremementlongue < 10)
+    {
+        putchar(javaisimplementenviedemettreunevariableextremementlongue + '0'); 
+        if (javaisimplementenviedemettreunevariableextremementlongue != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        javaisimplementenviedemettreunevariableextremementlongue++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
