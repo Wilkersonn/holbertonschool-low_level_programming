87b11be85008f028aba83789cca146946ac4b9ad
Wilkersonn <remyfournieroff@gmail.com>
2024-06-21 13:52:51 +0200

    Add README, task 0, header and _putchar.c .

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar \n");
+	
+	return (0);
+}
