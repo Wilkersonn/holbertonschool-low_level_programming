882b6e122d77d219bc2a427100b314b55111bb1c
Wilkersonn <remyfournieroff@gmail.com>
2024-06-21 10:39:57 +0200

    Push task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c <= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
