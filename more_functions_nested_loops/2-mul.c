08359db8c5f875609a08785aa7eeb9b35358e023
Wilkersonn <remyfournieroff@gmail.com>
2024-06-24 12:28:00 +0200

    Push task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * mul - Verify if the char is digit or not
+ *
+ * @a: first int to be multiplicate
+ * @b: second int to be multiplicate
+ *
+ * Return: 1 ou 0
+ *
+ */
+int mul(int a, int b)
+{
+	int calcul;
+
+	calcul = (a * b);
+		return (calcul);
+}
