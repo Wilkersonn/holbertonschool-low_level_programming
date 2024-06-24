1e557e196cd32ae4b467c7f9d50342bf8dd75232
Wilkersonn <remyfournieroff@gmail.com>
2024-06-24 12:00:47 +0200

    Push task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _isupper - Verify if the char is lower or not
+ *
+ * @c: the char being verified
+ *
+ * Return: 1 ou 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Mr robot reste et restera la meilleur série qui concerne la cybersecurité, les maladies mentales et autres. C'est long a démmarer, il faut s'accrocher pour comprendre. 
+
