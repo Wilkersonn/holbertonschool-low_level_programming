b9d8b15b87a1c7bf7b1e42cb51d437fb9d464281
Wilkersonn <remyfournieroff@gmail.com>
2024-06-26 13:33:39 +0200

    Push task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - know how char have a var char. 
+ *
+ * @a: first int to be swap
+ *
+ * @b: second int to be swap
+ */
+
+int _strlen(char *s)
+{
+
+	size_t longueur = strlen(s);
+	return (int)longueur;
+
+}
