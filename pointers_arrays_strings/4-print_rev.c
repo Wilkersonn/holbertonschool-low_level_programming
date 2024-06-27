d93f850f3d5a2369860a52775399844aebf7b0bb
Wilkersonn <remyfournieroff@gmail.com>
2024-06-27 15:32:30 +0200

    Push ty Arc

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _puts - Fonction permettant de print un str
+ * @str: string print
+ * Return: rien
+ */
+void print_rev(char *s)
+    {
+
+        int count;
+
+        count = 0;
+
+        while (*s != '\0')
+        /**
+         * Permet de traverser le array vers lequel point s
+         * "*str" désigne littéralement ce tableau je n'ai pas besoin de
+         * de son adresse mémoire avec p = &s ou autre.
+         */
+            {
+            s++;
+            count++;
+            }
+
+        while (count != 0)
+        {
+            _putchar(*s);
+            s--;
+            count--;  
+        }
+        
+
+    _putchar('\n');
+    return;
+    }
