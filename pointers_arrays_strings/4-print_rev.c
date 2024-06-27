5c7c8abc3623b4ca6b7f80b07bd6e8189a4256c6
Wilkersonn <remyfournieroff@gmail.com>
2024-06-27 15:38:29 +0200

    Push task

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,35 +1,35 @@
 #include "main.h"
 /**
- * _puts - Fonction permettant de print un str
+ * print_rev - Fonction permettant de print un str a lenvers
  * @s: string print
  * Return: rien
  */
 void print_rev(char *s)
-    {
+{
 
-        int count;
+	int count;
 
-        count = 0;
+	count = 0;
 
-        while (*s != '\0')
-        /**
-         * Permet de traverser le array vers lequel point s
-         * "*str" désigne littéralement ce tableau je n'ai pas besoin de
-         * de son adresse mémoire avec p = &s ou autre.
-         */
-            {
-            s++;
-            count++;
-            }
+	while (*s != '\0')
+	/**
+	 * Permet de traverser le array vers lequel point s
+	 * "*str" désigne littéralement ce tableau je n'ai pas besoin de
+	 * de son adresse mémoire avec p = &s ou autre.
+	 */
+		{
+		s++;
+		count++;
+		}
 
-        while (count != 0)
-        {
-            s--;
-            count--;
-            _putchar(*s);
-        }
-        
+	while (count != 0)
+	{
+		s--;
+		count--;
+		_putchar(*s);
+	}
 
-    _putchar('\n');
-    return;
-    }
+
+_putchar('\n');
+return;
+}
