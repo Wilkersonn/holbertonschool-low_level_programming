e0e5b1fae761313135cdd0a32f879065717c34d5
Wilkersonn <remyfournieroff@gmail.com>
2024-06-29 11:22:10 +0200

    Push task 6 with maybe the good answer this time

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
- * puts2 - Fonction permettant de print str une fois sur deux
- * en commencant par "0"
- *
- * @str: string that contain "0123456789"
- * Return: rien
+ * puts2 - Prints every other character of a string,
+ *         starting with the first character, followed by a new line.
+ * @str: The string to be processed.
  */
 void puts2(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i += 2)
-	_putchar(str[i]);
-
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
