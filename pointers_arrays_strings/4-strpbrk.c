d1400c386e522913d533d6bb59b2658e24e12a05
Wilkersonn <remyfournieroff@gmail.com>
2024-07-03 15:42:38 +0200

    Push

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to be searched
+ * @accept: the string containing the bytes to match
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (NULL);
+}
