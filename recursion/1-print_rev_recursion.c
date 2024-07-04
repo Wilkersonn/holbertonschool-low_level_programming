d19d5fab2ce80a8bdaaf9c5c1630cf3a28e0feef
Wilkersonn <remyfournieroff@gmail.com>
2024-07-04 14:39:42 +0200

    Push task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse, followed by a new line
+ * _print_rev_recursion - Prints a string in reverse
  * @s: The string to be printed in reverse
  *
  * Description: This function prints each character of the string recursively
- * in reverse order, followed by a new line.
+ * in reverse order.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
 }
