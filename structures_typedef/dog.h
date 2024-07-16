96dca6fe63f0f8c4e3cb0cfdf16655dd0334e7cf
Wilkersonn <remyfournieroff@gmail.com>
2024-07-16 10:12:10 +0200

    Push with correct betty coding style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - A dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
